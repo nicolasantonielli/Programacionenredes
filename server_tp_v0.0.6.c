6cc02d81855adf561581b2a6592fad39d2f60f7a
Nicolas Antonielli <nicolasantonielli@gmail.com>
2022-11-18 14:57:58 -0300

    a

--- server_tp_v0.0.6.c
+++ server_tp_v0.0.6.c
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
     memset(&server_addr, '0', sizeof(server_addr));
     memset(sendBuff, '0', sizeof(sendBuff));
 
-    //Configuracion de sockets
+    // Configuracion de sockets
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(PUERTO);
@@ -84,31 +84,27 @@ int main(int argc, char *argv[])
 // Funcion de los hilos
 void *atenderCliente(void *params)
 {
-    int pid = 0;
-    int ip = 0;
-    char sendBuff[8192];
+    char sendBuff[1024];
     char recvBuff[1024];
     int estadoRecibir = 0;
     int *descriptorHilo = 0;
     char body[2048];
     descriptorHilo = (int *)params;
     int length = 0;
-    char metodo[4];
-    char request[8192];
+    char request[1024];
     int bytesRecibidos = 0;
-    char headerImage[2048];
+
+    char imageHeader[2048];
     char imageBody[127];
 
-    int fimage = NULL;
+    FILE *fimage = NULL;
 
-    fimage = fopen("/home/nico/Documentos/2x2.png","r");
+    fimage = fopen("/home/nico/Documentos/2x2.png", "r");
 
-    /*
     memset(sendBuff, '0', sizeof(sendBuff));
-    memset(sendBuff, '0', sizeof(recvBuff));
-    // memset(request, '0', sizeof(request));
+    memset(recvBuff, '0', sizeof(recvBuff));
+    memset(request, '0', sizeof(request));
     memset(body, '0', sizeof(body));
-    memset(metodo, '0', sizeof(metodo));
 
     // recibo request
     do
@@ -134,10 +130,14 @@ void *atenderCliente(void *params)
         printf("Bytes recibidos: %d bytes\n", bytesRecibidos);
         printf("%s", request);
 
+        // Armo response
+
+        // length body
         length = 360;
-
-        sprintf(body, "<!DOCTYPE html\">\r\n<html>\r\n<head>\r\n<title>Servidor_Programacion en redes 2022</title>\r\n<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\" />\r\n</head>\r\n<body>\r\n<h2>Servidor TP Programacion en redes 2022</h2>\r\n<p>PID: %d</p><a href=\"http://127.0.0.1/image.jpg\">image.jpg</a>\r\n<p>IP del servidor: %d</p>\r\n<p>Descriptor numero: %d</p>\r\n</body>\r\n</html>", getpid(), ip, *descriptorHilo);
-
+        // body
+        bzero(sendBuff,sizeof(sendBuff));
+        sprintf(body, "<!DOCTYPE html\">\r\n<html>\r\n<head>\r\n<title>Servidor_Programacion en redes 2022</title>\r\n<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\" />\r\n</head>\r\n<body>\r\n<h2>Servidor TP Programacion en redes 2022</h2>\r\n<p>PID: %d</p><a href=\"http://192.168.199.101:8080/image.png\">image.png</a>\r\n<p>Descriptor numero: %d</p>\r\n</body>\r\n</html>", getpid(), *descriptorHilo);
+        // headers
         sprintf(sendBuff, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nAccept-Ranges: bytes\r\nServer: NicoServer_2.0\r\nDate: Sun, 13 Nov 2022 02:49:07 GMT\r\nContent-Length: %d\r\n\r\n", length);
         strcat(sendBuff, body);
 
@@ -145,30 +145,28 @@ void *atenderCliente(void *params)
         printf("\nDescriptor del hilo %d\n", *descriptorHilo);
         send(*descriptorHilo, sendBuff, sizeof(sendBuff), 0);
 
-
-text/html
-image/png
-image/jpeg
+        // text/html
+        // image/png
+        // image/jpeg
     } while (1);
-    */
-        sprintf(headerImage, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nDate: Sun, 13 Nov 2022 02:49:07 GMT\r\nContent-Length: 127\r\n\r\n");
-        //sprintf(imageBody,"1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567");
-        //strcpy(imageBody,(char*)fimage);
-        send(*descriptorHilo, headerImage, 102, 0);
 
-        while(fgets(imageBody, sizeof(imageBody), fimage) != NULL) {
-            if (send(*descriptorHilo, imageBody, sizeof(imageBody), 0) == -1) {
-            perror("[-]Error in sending file.");
-            exit(1);
-            }
-        bzero(imageBody, 127);
-        }
+    /*
+            sprintf(headerImage, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nDate: Sun, 13 Nov 2022 02:49:07 GMT\r\nContent-Length: 127\r\n\r\n");
+            //sprintf(imageBody,"1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567");
+            //strcpy(imageBody,(char*)fimage);
+            send(*descriptorHilo, imageHeader, 102, 0);
 
 
-        sendfile(*descriptorHilo,fimage,NULL,127);
-        printf("Enviando imagen\n");
+
+
+            sendfile(*descriptorHilo,fimage,NULL,127);
+            printf("Enviando imagen\n");
+        */
+
+    // Cierro Socket y archivo
+    close(*descriptorHilo);
     fclose(fimage);
+    return 0;
 }
 
-
-//https://idiotdeveloper.com/file-transfer-using-tcp-socket-in-c/
+// https://idiotdeveloper.com/file-transfer-using-tcp-socket-in-c/
